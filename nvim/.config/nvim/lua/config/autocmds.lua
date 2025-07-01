7590b16b47ce22942387db06e5e805d2013a55fd
Sammy Najib <sammy@prescryptive.com>
2025-07-01 14:18:38 -0500

    updating theme to poimandres

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@ vim.api.nvim_create_autocmd("FileType", {
     end)
   end,
 })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "*.ts", "*.tsx" },
+  callback = function()
+    vim.bo.shiftwidth = 4
+    vim.bo.tabstop = 4
+    vim.bo.softtabstop = 4
+  end,
+})
