84e8e9e1eb9088967c5907fc45c7f8fc56f27c7b
Sammy Najib <sammy@prescryptive.com>
2025-07-01 12:06:54 -0500

    update nvim colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,30 +1,13 @@
 return {
   {
-    "everviolet/nvim",
-    name = "evergarden",
+    "olivercederborg/poimandres.nvim",
+    name = "poimandres",
     priority = 1000,
-    opts = {
-      theme = {
-        variant = "fall",
-        accent = "green",
-      },
-      editor = {
-        transparent_background = true,
-        sign = { color = "none" },
-        float = {
-          color = "mantle",
-          invert_border = false,
-        },
-        completion = {
-          color = "surface0",
-        },
-      },
-    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "evergarden",
+      colorscheme = "poimandres",
     },
   },
 }
