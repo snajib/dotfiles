e6b3012189b8700283cc5ad1fa5f441a729c8528
snajib <sammy.najib@gmail.com>
2026-02-15 21:02:40 -0600

    small conf changes

--- nvim/.config/nvim/lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
--- Set 4-space tabs for TypeScript files
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = { "*.ts", "*.tsx" },
-  callback = function()
-    vim.bo.shiftwidth = 4
-    vim.bo.tabstop = 4
-    vim.bo.softtabstop = 4
-  end,
-})
